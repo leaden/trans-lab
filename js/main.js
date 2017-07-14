e17e785fc3284def52a76eefac676ead085b3966
maribel <maribel.diaz.j@gmail.com>
2017-07-14 10:38:15 -0400

    pantalla preguntas frecuentes accordion

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
 /* Holiii acá va tu código también */
 
+$(document).ready(function(){
+  $.$.ajax({
+  	url: 'http://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=22179163',
+  	type: 'GET',
+  	dataType: 'JSON',
+  })
+  .done(function() {
+  	console.log("success");
+  })
+  .fail(function() {
+  	console.log("error");
+  })
+  .always(function() {
+  	console.log("complete");
+  });
+  
+});
